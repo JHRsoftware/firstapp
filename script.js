94ea1349715f7c1a3f235e4c745e50322ff538d1
Jude Harshana <your-email@example.com>
2025-04-11 13:38:55 +0530

    new

--- script.js
+++ script.js
@@ -1,22 +1,17 @@
-document.getElementById("clientForm").addEventListener("submit", function(e) {
+document.querySelector("form").addEventListener("submit", function (e) {
     e.preventDefault();
-    const form = e.target;
-    const data = {
-      name: form.name.value,
-      address: form.address.value,
-      contact: form.contact.value
-    };
+    const formData = new FormData(e.target);
+    const data = Object.fromEntries(formData.entries());
   
     fetch("https://script.google.com/macros/s/AKfycbxe6LjyikzwB0aPInQvd1lEXZqrBQZT2ylB00AMSbb87vXsILLC8HSqu4p-mF_E0JuL/exec", {
-        method: "POST",
-        body: JSON.stringify(data),
-        headers: {
-          "Content-Type": "application/json"
-        }
-      })
-      
-    .then(res => res.text())
-    .then(response => alert(response))
-    .catch(error => console.error("Error!", error.message));
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+      .then(res => res.text())
+      .then(msg => alert("Submitted!"))
+      .catch(err => alert("Error: " + err.message));
   });
   
