d6487636b40090b45bc70088b4f9711bbea2d759
Jude Harshana <your-email@example.com>
2025-04-11 13:32:25 +0530

    d

--- script.js
+++ script.js
@@ -8,12 +8,13 @@ document.getElementById("clientForm").addEventListener("submit", function(e) {
     };
   
     fetch("https://script.google.com/macros/s/AKfycbxe6LjyikzwB0aPInQvd1lEXZqrBQZT2ylB00AMSbb87vXsILLC8HSqu4p-mF_E0JuL/exec", {
-      method: "POST",
-      body: JSON.stringify(data),
-      headers: {
-        "Content-Type": "application/json"
-      }
-    })
+        method: "POST",
+        body: JSON.stringify(data),
+        headers: {
+          "Content-Type": "application/json"
+        }
+      })
+      
     .then(res => res.text())
     .then(response => alert(response))
     .catch(error => console.error("Error!", error.message));
