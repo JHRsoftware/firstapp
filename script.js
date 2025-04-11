2747d836e12876d61260f5f6354bdbcb6c6cb1cd
Jude Harshana <your-email@example.com>
2025-04-11 14:26:57 +0530

    de

--- script.js
+++ script.js
@@ -21,3 +21,26 @@ document.getElementById("clientForm").addEventListener("submit", function(e) {
     });
   });
   
+//////////////////////////////////////////////////////////////////
+
+  document.getElementById("itemForm").addEventListener("submit", function(e) {
+    e.preventDefault();
+    const form = e.target;
+    const data = new URLSearchParams();
+    data.append("itemName", form.itemName.value);
+    data.append("price", form.price.value);
+  
+    fetch("https://script.google.com/macros/s/AKfycbxe6LjyikzwB0aPInQvd1lEXZqrBQZT2ylB00AMSbb87vXsILLC8HSqu4p-mF_E0JuL/exec", {
+      method: "POST",
+      body: data
+    })
+    .then(res => res.text())
+    .then(response => {
+      document.getElementById("itemResponse").innerText = response;
+      form.reset();
+    })
+    .catch(error => {
+      document.getElementById("itemResponse").innerText = "Error: " + error.message;
+    });
+  });
+  
